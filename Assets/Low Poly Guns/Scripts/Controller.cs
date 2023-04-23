48fedf622b097395203271f4909b3e0a8b1950a7
Rafael Coutinho <rafaeljcoutinhoo@gmail.com>
2023-04-23 11:56:29 -0300

    wip

---
+++ Assets/Low Poly Guns/Scripts/Controller.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FirstPersonCameraController : MonoBehaviour
+{
+    public float mouseSensitivity = 100.0f;
+    public Transform playerBody;
+
+    private float xRotation = 0.0f;
+
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90.0f, 90.0f);
+
+        transform.localRotation = Quaternion.Euler(xRotation, 0.0f, 0.0f);
+        playerBody.Rotate(Vector3.up * mouseX);
+    }
+}
+
+
