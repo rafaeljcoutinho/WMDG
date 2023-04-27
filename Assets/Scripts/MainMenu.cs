66c1a4d918f2def5a2d7fe907bc3bd2de6edc77e
Rafael Coutinho <rafaeljcoutinhoo@gmail.com>
2023-04-27 18:37:42 -0300

    adding new target model, new menu scene, fixing HUD

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MainMenu : MonoBehaviour
+{
+    public Button startGameButton;
+    public Button viewScoreButton;
+    public Button exitGameButton;
+
+    private void Start()
+    {
+        startGameButton.onClick.AddListener(StartGame);
+        viewScoreButton.onClick.AddListener(ViewScore);
+        exitGameButton.onClick.AddListener(ExitGame);
+    }
+
+    private void StartGame()
+    {
+        SceneManager.LoadScene("GameScene"); // Replace "GameScene" with the actual name of your game scene.
+    }
+
+    private void ViewScore()
+    {
+        SceneManager.LoadScene("ScoreScene"); // Replace "ScoreScene" with the actual name of your score scene.
+    }
+
+    private void ExitGame()
+    {
+        Application.Quit();
+    }
+}
