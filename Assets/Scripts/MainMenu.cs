1d6c84b212d0df545aac2fe1a04843f83da913f8
Rafael Coutinho <rafaeljcoutinhoo@gmail.com>
2023-04-28 16:27:39 -0300

    adding PlayerData, and refactor ScoreManager

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -3,22 +3,35 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
+using TMPro;
 
 public class MainMenu : MonoBehaviour
 {
-    public Button startGameButton;
-    public Button viewScoreButton;
-    public Button exitGameButton;
+    [SerializeField] private TMP_InputField playerNameInput;
+    [SerializeField] private GameObject initialMenu;
+    [SerializeField] private Button startGameButton;
+    [SerializeField] private Button playGameButton;
+    [SerializeField] private Button viewScoreButton;
+    [SerializeField] private Button exitGameButton;
 
     private void Start()
     {
         startGameButton.onClick.AddListener(StartGame);
         viewScoreButton.onClick.AddListener(ViewScore);
         exitGameButton.onClick.AddListener(ExitGame);
+        playGameButton.onClick.AddListener(PlayGame);
     }
 
     private void StartGame()
     {
+        initialMenu.SetActive(false);
+        playerNameInput.gameObject.SetActive(true);
+
+    }
+    private void PlayGame()
+    {
+        PlayerData.Instance.SetPlayerName(playerNameInput.text);
+        Debug.Log(PlayerData.Instance.PlayerName);
         SceneManager.LoadScene("GameScene"); // Replace "GameScene" with the actual name of your game scene.
     }
 
