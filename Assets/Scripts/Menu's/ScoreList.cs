9ad049a5e89c21d5ca082f2f94e02242a3289ca2
Rafael Coutinho <rafaeljcoutinhoo@gmail.com>
2023-05-08 10:11:31 -0300

    finish score controller and visual

--- Assets/Scripts/Menu's/ScoreList.cs
+++ Assets/Scripts/Menu's/ScoreList.cs
@@ -1,18 +1,47 @@
-using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class ScoreList : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public SaveMethod saveMethod;
+    public GameObject scoreEntryPrefab;
+    public Transform contentTransform;
+
     void Start()
     {
-        
+        PopulateScoreList();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void PopulateScoreList()
     {
-        
+        List<Dictionary<string, int>> scores = saveMethod.LoadScores();
+        List<KeyValuePair<string, int>> playerScores = new List<KeyValuePair<string, int>>();
+
+        // Adiciona todos os pares chave-valor na lista playerScores
+        foreach (Dictionary<string, int> score in scores)
+        {
+            foreach (KeyValuePair<string, int> entry in score)
+            {
+                playerScores.Add(entry);
+            }
+        }
+
+        // Ordena a lista de pares chave-valor pela pontuação, em ordem decrescente
+        playerScores = playerScores.OrderByDescending(x => x.Value).ToList();
+
+        // Adiciona as entradas ordenadas à lista
+        foreach (KeyValuePair<string, int> playerScoreData in playerScores)
+        {
+            GameObject newEntry = Instantiate(scoreEntryPrefab, contentTransform);
+            newEntry.GetComponent<ScoreEntryController>().SetScore(playerScoreData.Key, playerScoreData.Value);
+        }
+        ResetScrollPosition();
+    }
+    public void ResetScrollPosition()
+    {
+        GetComponent<ScrollRect>().verticalNormalizedPosition = 1f;
     }
 }
