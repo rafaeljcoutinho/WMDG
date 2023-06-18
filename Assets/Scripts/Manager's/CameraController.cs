8e57fbc4932d9c9a2be620962defd78ae8034860
Rafael Coutinho <rafaeljcoutinhoo@gmail.com>
2023-06-18 14:40:30 -0300

    adding recoilSystem and ammoSystem

--- Assets/Scripts/Manager's/CameraController.cs
+++ Assets/Scripts/Manager's/CameraController.cs
@@ -4,8 +4,8 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
-    [SerializeField]
-    private float mouseSensitivity = 100f;
+    [SerializeField] private float mouseSensitivity = 100f;
+    [SerializeField] private WeaponManager weaponManager;
 
     private Transform playerBody;
     private float xRotation = 0f;
@@ -25,6 +25,9 @@ public class CameraController : MonoBehaviour
     {
         float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
         float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
+        mouseX += weaponManager.CurrentRecoil.y;
+        mouseY -= weaponManager.CurrentRecoil.x;
 
         xRotation -= mouseY;
         xRotation = Mathf.Clamp(xRotation, -90f, 90f);
